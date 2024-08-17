7d90c67b5fd62674155b793cb44a9e1b035bf8a7
Gus Meyer <gus@robotics88.com>
2024-08-17 02:29:35 +0000

    Fixing setup workspace for docker workflow

--- setup_workspace.sh
+++ setup_workspace.sh
@@ -9,26 +9,8 @@ if [ "$input" != "s" ] && [ "$input" != "d" ]; then
     exit 1
 fi
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install curl
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install python3-vcstool
-
-echo "source /home/$USER/src/distal_ros2/install/setup.bash --extend" >> /home/$USER/.bashrc
-echo "source /home/$USER/src/ws_livox/install/setup.bash --extend" >> /home/$USER/.bashrc
-
-cd src
-
 if [ "$input" == "s" ]; then
     vcs import < simulation.repos
 elif [ "$input" == "d" ]; then
     vcs import < decco.repos
-    catkin config --cmake-args -DCMAKE_BUILD_TYPE=Release
 fi
-
-sudo ./mavros/mavros/scripts/install_geographiclib_datasets.sh
-
-cd ..
-
-rosdep install --from-paths src --ignore-src -r -y
