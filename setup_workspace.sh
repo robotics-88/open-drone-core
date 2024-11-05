b9f576d3fe6ae143def9bc5c3d5dcd1499eb5ea8
Gus Meyer <gus@robotics88.com>
2024-11-05 12:23:44 -0800

    Only set manual IP if on decco

--- setup_workspace.sh
+++ setup_workspace.sh
@@ -87,8 +87,10 @@ sudo cp ~/src/distal/src/vehicle-launch/config/99-r88.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules && sudo udevadm trigger
 sudo usermod -a -G dialout $USER
 
-sudo nmcli con mod "Wired connection 1" ipv4.addresses "192.168.1.5/24" ipv4.gateway "192.168.1.1" ipv4.method "manual"
-sudo route add 192.168.1.12 eth0
+if [ "$1" == "-d" ]; then
+    sudo nmcli con mod "Wired connection 1" ipv4.addresses "192.168.1.5/24" ipv4.gateway "192.168.1.1" ipv4.method "manual"
+    sudo route add 192.168.1.12 eth0
+fi
 
 echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 echo "source /home/$USER/src/livox_ros_driver2/install/setup.bash" >> ~/.bashrc
