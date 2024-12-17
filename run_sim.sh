cc85256ab241d01b0e9aec65956c76ef244201d9
Gus Meyer <gus@robotics88.com>
2024-12-17 13:26:05 -0800

    Changing launch line to be standard

--- run_sim.sh
+++ run_sim.sh
@@ -6,4 +6,4 @@ source /home/$USER/src/distal/install/setup.bash
 export AIRSIM_DIR="/home/$USER/src/Colosseum"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
-ros2 launch src/vehicle-launch/launch/decco.launch simulate:=true $@
+ros2 launch vehicle_launch decco.launch simulate:=true $@
