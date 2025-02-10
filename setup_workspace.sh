94d5634b4a135165fa99deccfd7d3711e53dd36d
Quinn Bowers <quinn@robotics88.com>
2025-02-10 13:16:26 -0500

    Add new dependencies

--- setup_workspace.sh
+++ setup_workspace.sh
@@ -30,6 +30,18 @@ sudo apt install -y python3-rosdep python3-vcstool python3-colcon-common-extensi
 sudo rosdep init
 rosdep update
 
+
+# Install clang compiler and other optimizations
+sudo apt install -y clang lld libomp-dev ccache git-lfs
+colcon mixin add default https://raw.githubusercontent.com/colcon/colcon-mixin-repository/master/index.yaml
+colcon mixin update default
+git lfs install
+
+# Fetch git lfs artifacts, if not present already
+cd $DISTAL_DIR
+git lfs fetch && git lfs pull
+
+
 # Pull in repos
 cd $DISTAL_DIR/src/
 if [ "$1" == "-s" ]; then
