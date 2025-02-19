f51341f8ed3af3cd553da1f0a761df1e91e00c32
Gus Meyer <gus@robotics88.com>
2025-02-19 14:31:07 -0800

    Updating dev repos

--- run_sim.sh
+++ run_sim.sh
@@ -4,7 +4,6 @@ source /opt/ros/humble/setup.bash
 source /home/$USER/src/livox_ws/install/setup.bash
 source /home/$USER/src/distal/install/setup.bash
 export AIRSIM_DIR="/home/$USER/src/Colosseum"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
 # Get UTC date for logging
 date=$(date -u '+%Y-%m-%d_%H-%M-%S')
