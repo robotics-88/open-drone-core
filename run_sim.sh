a362a09b1be2d3ecc716fa5ad3f67a0611739408
Gus Meyer <gus@robotics88.com>
2025-02-19 12:32:21 -0800

    Updating data directory in run_sim

--- run_sim.sh
+++ run_sim.sh
@@ -6,4 +6,12 @@ source /home/$USER/src/distal/install/setup.bash
 export AIRSIM_DIR="/home/$USER/src/Colosseum"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
-ros2 launch vehicle_launch decco.launch simulate:=true $@
+# Get UTC date for logging
+date=$(date -u '+%Y-%m-%d_%H-%M-%S')
+data_directory=/home/$USER/r88_public/records/$date/
+mkdir -p $data_directory
+
+stdbuf -oL ros2 launch vehicle_launch decco.launch \
+    simulate:=true \
+    data_directory:=$data_directory \
+    $@ 2>&1 | tee $data_directory/distal_stdout_$date.log
