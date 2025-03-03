1dbad535e7bd966a0626392ac13959cca25f7a75
Gus Meyer <gus@robotics88.com>
2025-03-03 00:15:08 +0000

    Fixing redudnant actions in setup

--- setup_workspace.sh
+++ setup_workspace.sh
@@ -107,13 +107,6 @@ elif [[ "$1" == "-e" ]]; then
     sudo usermod -a -G dialout $USER
 fi
 
-sudo udevadm control --reload-rules && sudo udevadm trigger
-sudo usermod -a -G dialout $USER
-
-if [[ "$1" == "-d" ]]; then
-    sudo nmcli con mod "Wired connection 1" ipv4.addresses "192.168.1.5/24" ipv4.gateway "192.168.1.1" ipv4.method "manual"
-fi
-
 echo "source /opt/ros/humble/setup.bash" >> $HOME/.bashrc
 echo "source $LIVOX_DIR/install/setup.bash" >> $HOME/.bashrc
 echo "source $DISTAL_DIR/install/setup.bash" >> $HOME/.bashrc
