45eb3f2a1242221eca0b388215af38e1a8b73d8f
Gus Meyer <gus@robotics88.com>
2025-03-09 06:14:49 +0000

    Update to logging names

--- run_ecco.sh
+++ run_ecco.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-# Start ros nodes
 source /opt/ros/humble/setup.bash
 source /home/$USER/src/distal/install/setup.bash
 source /home/$USER/src/livox_ros_driver2/install/setup.bash
 
 # Get UTC date for logging
 date=$(date -u '+%Y-%m-%d_%H-%M-%S')
-
-data_directory=/home/$USER/logs/ros_logs/$date/
-
+data_directory=/home/$USER/r88_public/records/$date/
 mkdir -p $data_directory
 
 # Launch code
 stdbuf -oL ros2 launch vehicle_launch ecco.launch \
-    data_directory:=$data_directory $@ 2>&1 | tee $data_directory/stdout.log
+    data_directory:=$data_directory $@ 2>&1 | tee $data_directory/distal_stdout_$date.log
