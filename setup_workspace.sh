775e6e936b62f6a010f6de03673a5c60c1c0c346
rien88 <112721833+rien88@users.noreply.github.com>
2025-03-18 13:32:53 -0400

    Fixed syntax and new repo files name for sim (#20)
    * Fixed syntax and new repo files name for sim
    
    * Uncommenting, oops
    
    * Removed extra hash

--- setup_workspace.sh
+++ setup_workspace.sh
@@ -44,12 +44,14 @@ git lfs fetch && git lfs pull
 
 # Pull in repos
 cd $DISTAL_DIR/src/
-if [[ "$1" == "-s" ]]; then
-    vcs import < privileged_developer.repos
-elif [[ "$1" == "-d" ]]; then
+if [[ "$1" == "-s" ]]
+  then
+    vcs import < sim_full.repos
+elif [[ "$1" == "-d" ]]
+  then
     vcs import < decco.repos
-fi
-elif [[ "$1" == "-e" ]]; then
+elif [[ "$1" == "-e" ]]
+  then
     vcs import < ecco.repos
 fi
 vcs pull
@@ -88,20 +90,24 @@ sudo ./install_geographiclib_datasets.sh
 
 # Install seek sdk
 cd $DISTAL_DIR
-if [ "$1" == "-s" ]; then
+if [ "$1" == "-s" ]
+  then
     sudo apt install -y ./assets/seekthermal-sdk-dev-4.4.2.20_amd64.deb
-elif [[ "$1" == "-d" || "$1" == "-e" ]]; then
+elif [[ "$1" == "-d" || "$1" == "-e" ]]
+  then
     sudo apt install -y ./assets/seekthermal-sdk-dev-4.4.2.20_arm64.deb
 fi
 
 
 # Other config
-if [[ "$1" == "-d"]]; then
+if [[ "$1" == "-d"]]
+  then
     sudo cp $DISTAL_DIR/src/vehicle-launch/config/99-decco.rules /etc/udev/rules.d/
     sudo udevadm control --reload-rules && sudo udevadm trigger
     sudo usermod -a -G dialout $USER
     sudo nmcli con mod "Wired connection 1" ipv4.addresses "192.168.1.5/24" ipv4.gateway "192.168.1.1" ipv4.method "manual"
-elif [[ "$1" == "-e" ]]; then
+elif [[ "$1" == "-e" ]]
+  then
     sudo cp $DISTAL_DIR/src/vehicle-launch/config/99-ecco.rules /etc/udev/rules.d/
     sudo udevadm control --reload-rules && sudo udevadm trigger
     sudo usermod -a -G dialout $USER
@@ -111,6 +117,7 @@ echo "source /opt/ros/humble/setup.bash" >> $HOME/.bashrc
 echo "source $LIVOX_DIR/install/setup.bash" >> $HOME/.bashrc
 echo "source $DISTAL_DIR/install/setup.bash" >> $HOME/.bashrc
 
-if [[ "$1" == "-s" ]]; then
+if [[ "$1" == "-s" ]]
+  then
     echo "export AIRSIM_DIR="$HOME/src/Colosseum"" >> $HOME/.bashrc
 fi
