14758ebe668dc737993462f9245b40993256d493
Gus Meyer <gus@robotics88.com>
2025-04-03 18:02:28 -0700

    Adding more gazebo install steps

--- setup_workspace.sh
+++ setup_workspace.sh
@@ -90,11 +90,25 @@ cd $DISTAL_DIR
 if [[ "$1" == "-s" ]]; then
     sudo apt install -y $DISTAL_DIR/assets/seekthermal-sdk-dev-4.4.2.20_amd64.deb
     sudo apt install -y libexiv2-dev libimage-exiftool-perl exif exiv2
+
+    echo "export AIRSIM_DIR="$HOME/src/Colosseum"" >> $HOME/.bashrc
+
+    # Install Gazebo
+    sudo curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install gz-harmonic
     sudo apt install libgz-sim8-dev rapidjson-dev
     sudo apt install libopencv-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl
-    echo "export AIRSIM_DIR="$HOME/src/Colosseum"" >> $HOME/.bashrc
     echo 'export GZ_SIM_SYSTEM_PLUGIN_PATH=$HOME/gz_ws/src/ardupilot_gazebo/build:${GZ_SIM_SYSTEM_PLUGIN_PATH}' >> ~/.bashrc
     echo 'export GZ_SIM_RESOURCE_PATH=$HOME/gz_ws/src/ardupilot_gazebo/models:$HOME/gz_ws/src/ardupilot_gazebo/worlds:${GZ_SIM_RESOURCE_PATH}' >> ~/.bashrc
+
+    mkdir -p $HOME/gz_ws/src && cd $HOME/gz_ws/src
+    export GZ_VERSION=harmonic
+    cd ardupilot_gazebo
+    mkdir build && cd build
+    cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
+    make -j4
 elif [[ "$1" == "-d" ]]; then
     sudo apt install -y $DISTAL_DIR/assets/seekthermal-sdk-dev-4.4.2.20_arm64.deb
     sudo cp $DISTAL_DIR/src/vehicle-launch/config/99-decco.rules /etc/udev/rules.d/
