7d1c6e020e6fd53d3572d1108d01e3ed474e6516
Rien88 <erin@robotics88.com>
2025-06-19 14:12:16 -0400

    Updated run drone and run sim scripts to use new launch file, removed ecco and decco separate scripts

--- run_drone.sh
+++ run_drone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source /opt/ros/humble/setup.bash
-source /home/$USER/src/distal/install/setup.bash
+source /home/$USER/src/open-drone-core/install/setup.bash
 source /home/$USER/src/livox_ros_driver2/install/setup.bash
 
 # Get UTC date for logging
@@ -9,6 +9,6 @@ date=$(date -u '+%Y-%m-%d_%H-%M-%S')
 data_directory=/home/$USER/r88_public/records/$date/
 mkdir -p $data_directory
 
-stdbuf -oL ros2 launch vehicle_launch decco.launch \
-    enable_autonomy:=true tilted_lidar:=true \
+stdbuf -oL ros2 launch vehicle_launch opendrone.launch.py \
+    config_file:=decco.yaml \
     data_directory:=$data_directory $@ 2>&1 | tee $data_directory/distal_stdout_$date.log
--- run_sim.sh
+++ run_sim.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
 
 source /opt/ros/humble/setup.bash
-source /home/$USER/src/distal/install/setup.bash
+source /home/$USER/src/open-drone-core/install/setup.bash
 source /home/$USER/src/livox_ros_driver2/install/setup.bash
 export AIRSIM_DIR="/home/$USER/src/Colosseum"
+
+# default config
+config_file="gazebo.yaml"
+
+# detect if user explicitly passed config_file:=… (so we don’t stomp it),
+# and also if they asked for do_airsim:=true
+config_override=false
+for arg in "$@"; do
+  case "$arg" in
+    config_file:*)  
+      config_override=true
+      ;;
+    do_airsim:=true)
+      config_file="airsim.yaml"
+      ;;
+  esac
+done
 
 # Get UTC date for logging
 date=$(date -u '+%Y-%m-%d_%H-%M-%S')
 data_directory=/home/$USER/r88_public/sim/$date/
-mkdir -p $data_directory
+mkdir -p "$data_directory"
 
-stdbuf -oL ros2 launch vehicle_launch decco.launch \
-    simulate:=true \
-    data_directory:=$data_directory $@ 2>&1 | tee $data_directory/distal_stdout_$date.log
+# build the ros2-launch invocation
+cmd=( ros2 launch vehicle_launch opendrone.launch.py
+      simulate:=true
+)
+
+# only inject our default if they didn’t override it
+if ! $config_override ; then
+  cmd+=( config_file:="$config_file" )
+fi
+
+cmd+=( data_directory:="$data_directory" "${@}" )
+
+# run & tee
+stdbuf -oL "${cmd[@]}" 2>&1 | tee "$data_directory/distal_stdout_$date.log"
