016393d35b19153273d40437603a33d9f7924923
Rien88 <erin@robotics88.com>
2025-07-01 15:34:01 -0400

    Bugfix on bash command format

--- quickstart.sh
+++ quickstart.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 echo "🏗️  Running workspace setup…"
-bash "$SCRIPT_DIR/setup_workspace.sh -s"
+bash "$SCRIPT_DIR/setup_workspace.sh" -s
 
 echo "📦  Building with colcon…"
 colcon build --packages-skip airsim_launch
